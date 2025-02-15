f5f62e51d5ba3b9ea73ee0a84431294425c69919
aatuuss <slnikmatus@gmail.com>
2025-02-15 15:06:50 +0700

    data-alat 1

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -64,7 +64,7 @@ const Navbar = () => {
             <ul className="font-normal flex flex-row space-x-8">
               <li>
                 <a
-                  href="#"
+                  href="/data-pelanggan"
                   className="block py-2 px-3 text-lg text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
                   aria-current="page"
                 >
@@ -73,10 +73,10 @@ const Navbar = () => {
               </li>
               <li>
                 <a
-                  href="#"
+                  href="/data-alat"
                   className="block py-2 px-3 text-lg text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
                 >
-                  Sewa Alat
+                  Data Alat
                 </a>
               </li>
             </ul>
