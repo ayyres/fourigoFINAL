14e555ffd5b260693ae5bdc741f35dc565a84f04
aatuuss <slnikmatus@gmail.com>
2025-02-17 08:15:35 +0700

    new dashboard

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,16 +6,21 @@ const Navbar = () => {
       <nav className="fixed top-0 left-0 w-full bg-white border-b border-gray-300 dark:bg-gray-900 z-50">
         <div className="max-w-screen-2xl flex items-center justify-between mx-5 p-4">
           {/* Logo di Pojok Kiri */}
-          <Link href="https://flowbite.com/">
-            <img
-              src="/logo.jpg"
-              className="max-h-28 object-contain" 
-              alt="Rentronix"
-            />
-          </Link>
+          <div className="">
+            <Link href="/dashboard">
+              <img
+                src="/logo.jpg"
+                className="max-h-48 max-w-48 object-contain"
+                alt="Rentronix"
+              />
+            </Link>
+          </div>
 
           {/* Menu di Tengah */}
-          <div className="hidden md:flex justify-start w-full ml-8" id="navbar-default">
+          <div
+            className="hidden md:flex justify-start w-full ml-8"
+            id="navbar-default"
+          >
             <ul className="font-normal flex flex-row space-x-8">
               <li>
                 <Link href="/dashboard/admin/data-pelanggan">
@@ -67,32 +72,6 @@ const Navbar = () => {
               </button>
             </div>
           </form>
-
-          {/* Menu di Tengah */}
-          <div
-            className="hidden md:flex justify-center w-full"
-            id="navbar-default"
-          >
-            <ul className="font-normal flex flex-row space-x-8">
-              <li>
-                <a
-                  href="dashboard/admin/data-pelanggan"
-                  className="block py-2 px-3 text-lg text-white bg-blue-700 rounded-sm md:bg-transparent md:text-blue-700 md:p-0 dark:text-white md:dark:text-blue-500"
-                  aria-current="page"
-                >
-                  Data Pelanggan
-                </a>
-              </li>
-              <li>
-                <a
-                  href="dashboard/admin/data-alat"
-                  className="block py-2 px-3 text-lg text-gray-900 rounded-sm hover:bg-gray-100 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 dark:text-white md:dark:hover:text-blue-500 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent"
-                >
-                  Data Alat
-                </a>
-              </li>
-            </ul>
-          </div>
         </div>
       </nav>
       {/* Tambahkan padding di atas konten agar tidak tertutup navbar */}
