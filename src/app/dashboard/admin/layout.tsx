3dce4cfb4cf4029caec01c55bad0dcd754c5b66b
aatuuss <slnikmatus@gmail.com>
2025-02-18 09:08:49 +0700

    neww

--- src/app/dashboard/admin/layout.tsx
+++ src/app/dashboard/admin/layout.tsx
@@ -3,6 +3,7 @@ import { Inter, Roboto_Mono } from "next/font/google";
 import Navbar from "@/components/Navbar"; // Pastikan path ke Navbar sudah benar
 import QueryProvider from "@/providers/QueryProvider";
 import { AuthProvider } from "@/context/AuthContext";
+import Footer from "../Footer";
 
 const inter = Inter({
   variable: "--font-inter",
